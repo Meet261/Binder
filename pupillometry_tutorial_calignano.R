3fb0646bd597bb3eeb0ce980341dd3dbfd44592c
Meet261 <meetkumarmangroliya5@gmail.com>
2024-09-17 10:55:16 +0200

    Update pupillometry_tutorial_calignano.R

--- pupillometry_tutorial_calignano.R
+++ pupillometry_tutorial_calignano.R
@@ -2,9 +2,8 @@ library(dplyr)
 library(naniar)
 library(plyr)
 library(mgcv)
-library(itsadug)
 library(sjPlot)
-
+install.packages('itsadug', repos='http://cran.rstudio.com/')
 # Assuming 'data' is a dataframe already loaded from 'dataset_tutorial.csv'
 data <- data[complete.cases(data$average), ]
 
