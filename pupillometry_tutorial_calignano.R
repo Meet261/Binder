f5f4496add2ece3473f9b8218ed244a127e23bbe
Meet261 <meetkumarmangroliya5@gmail.com>
2024-09-17 10:46:37 +0200

    Update pupillometry_tutorial_calignano.R

--- pupillometry_tutorial_calignano.R
+++ pupillometry_tutorial_calignano.R
@@ -5,6 +5,7 @@ library(mgcv)
 library(itsadug)
 library(sjPlot)
 
+# Assuming 'data' is a dataframe already loaded from 'dataset_tutorial.csv'
 data <- data[complete.cases(data$average), ]
 
 summary(data)
